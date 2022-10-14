34bcf2da93e1e978db8d53a5b7429d5cc419d81e
diego <diego.martignoni@abenergie.it>
2022-10-14 02:18:36 +0200

    registering dbcontext

--- ParticipationPortal.API/Program.cs
+++ ParticipationPortal.API/Program.cs
@@ -1,11 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using ParticipationPortal.Infrastructure;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContextPool<ParticipationPortalContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("participation_portal_test"), serverOptions =>
+    {
+        serverOptions.MigrationsAssembly
+        (typeof(Program).Assembly.FullName);
+        serverOptions.CommandTimeout(600);
+    });
+});
 
 var app = builder.Build();
 

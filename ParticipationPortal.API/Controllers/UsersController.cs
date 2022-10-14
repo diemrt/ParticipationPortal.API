33dbb11696f9fb7e41451ea761a6d36b488a58ff
diego <diego.martignoni@abenergie.it>
2022-10-14 15:40:33 +0200

    adding basic users controller

---
+++ ParticipationPortal.API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParticipationPortal.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Find))]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Create))]
+        public IActionResult Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

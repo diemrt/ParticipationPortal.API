e3f2bcdce05c1b711dd39ed232eea9bfd36e817e
diego <diego.martignoni@abenergie.it>
2022-10-14 02:38:34 +0200

    fixing settings

--- ParticipationPortal.API/Program.cs
+++ ParticipationPortal.API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<ParticipationPortalContext>(options =>
 {
-    options.UseSqlite(builder.Configuration.GetConnectionString("participation_portal_test"), serverOptions =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ParticipationPortal"), serverOptions =>
     {
         serverOptions.MigrationsAssembly
         (typeof(Program).Assembly.FullName);
